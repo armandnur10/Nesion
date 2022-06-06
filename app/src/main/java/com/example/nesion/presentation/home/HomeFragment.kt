2b05777006765c493d5e52d4f817facef732e845
thaariqnst <thaariqnst@gmail.com>
2022-06-06 09:03:45 +0700

    Thaariq : Fix my_nav.xml & HomeFragment.kt

--- app/src/main/java/com/example/nesion/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/nesion/presentation/home/HomeFragment.kt
@@ -14,16 +14,6 @@ import com.example.nesion.databinding.FragmentHomeBinding
 import com.example.nesion.presentation.MainViewModel
 import com.example.nesion.presentation.NewsAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding?=null
@@ -46,7 +36,7 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         _binding = FragmentHomeBinding.inflate(layoutInflater)
 
         return binding.root
