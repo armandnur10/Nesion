e739d81a73baa8075a47ae60733e2b20837ee23e
armandnur10 <zxuanggg@gmail.com>
2022-06-06 13:40:48 +0700

    commit

--- app/src/main/java/com/example/nesion/presentation/MainActivity.kt
+++ app/src/main/java/com/example/nesion/presentation/MainActivity.kt
@@ -5,18 +5,25 @@ import android.os.Bundle
 import androidx.navigation.findNavController
 import com.example.nesion.R
 import com.example.nesion.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_container)
-        return super.onSupportNavigateUp() || navController.navigateUp()
+        binding.vpNews.adapter= SectionPagerAdapter(this)
+        binding.toolbar.elevation = 0f
+        binding.appBar.elevation = 0f
+
+        val tablist = arrayOf("News", "Tip", "Recommend")
+        TabLayoutMediator(binding.tabs, binding.vpNews){
+                tab, position -> tab.text = tablist[position]
+        }.attach()
     }
 }
