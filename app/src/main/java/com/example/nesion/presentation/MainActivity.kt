f7a3b15a63a3db6590a16cd43f197239027eddc9
Harits <haritsdeveloper@gmail.com>
2022-06-06 09:55:27 +0700

    Navigation Correctly

--- app/src/main/java/com/example/nesion/presentation/MainActivity.kt
+++ app/src/main/java/com/example/nesion/presentation/MainActivity.kt
@@ -8,11 +8,9 @@ import com.example.nesion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
     }
